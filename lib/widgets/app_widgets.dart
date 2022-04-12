88debf3a87bf3e272f424f91129b287a7436a9c1
Abdulkadir Eyigül <aeyigul@outlook.com>
2022-04-12 17:43:34 +0300

    movie card updated

--- lib/widgets/app_widgets.dart
+++ lib/widgets/app_widgets.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 
 class AppWidgets {
   static Widget movieCard(Map movie, context) {
-    return Container(
-      margin: const EdgeInsets.only(right: 20),
-      child: GestureDetector(
-        onTap: () => {
-          Navigator.pushNamed(context, 'movie_detail_screen', arguments: {
-            'movie': movie,
-          }),
-        },
+    return GestureDetector(
+      onTap: () => {
+        Navigator.pushNamed(context, 'movie_detail_screen', arguments: {
+          'movie': movie,
+        }),
+      },
+      child: Card(
+        margin: const EdgeInsets.only(right: 20),
         child: ClipRRect(
-          borderRadius: BorderRadius.circular(5), //add border radius
+          borderRadius: BorderRadius.circular(5),
           child: Image.network(
             movie['image'],
             width: 200,
@@ -29,10 +29,8 @@ class AppWidgets {
       title: Text(movie['name']),
       subtitle: Text(movie['year'].toString()),
       onTap: () => {
-        Navigator.pushNamed(context, 'movie_detail_screen', arguments: {
-          'movie': movie,
-          'id': id
-        }),
+        Navigator.pushNamed(context, 'movie_detail_screen',
+            arguments: {'movie': movie, 'id': id}),
       },
     );
   }
